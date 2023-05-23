b6ec1bdf59a9604f35f15efca9139096ca4554a0
Dudi1896 <denzelu181@gmail.com>
2023-05-22 23:32:42 -0400

    Setup graphql endpoint- queries sent to front end

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default function Home() {
+import client from '@/src/apollo/client';
+import { gql } from '@apollo/client';
+
+export default function Home(props) {
+  console.log('Props-1: ', props)
   return <div>Next JS &amp; WordPress course.</div>;
+}
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+    query PagesQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }`,
+  })
+  return{
+    props: {
+      data,
+      myexampleprop: "test"
+    }
+  }
 }
