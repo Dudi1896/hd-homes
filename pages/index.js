8d73eaa830989283a854e3ca384c814022d2a939
Dudi1896 <denzelu181@gmail.com>
2023-05-31 00:25:37 -0400

    Menu component + CTA Button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import client from "@/src/apollo/client";
 import { BlockRenderer } from "@/src/components/BlockRenderer";
+import { MainMenu } from "@/src/components/MainMenu";
 import { cleanAndTransformBlocks } from "@/utils/cleanAndTransformBlocks";
-import { mapMainMenuItems } from '@/utils/mapMainMenuItems';
+import { mapMainMenuItems } from "@/utils/mapMainMenuItems";
 import { gql } from "@apollo/client";
 
 export default function Home(props) {
   console.log("Props-1: ", props);
   return (
     <div>
+      <MainMenu
+        items={props.mainMenuItems}
+        callToActionDestination={props.callToActionDestination}
+        callToActionLabel={props.callToActionLabel}
+      />
       <BlockRenderer blocks={props.blocks} />
     </div>
   );
@@ -25,6 +31,15 @@ export const getStaticProps = async () => {
         }
         acfOptionsMainMenu {
           mainMenu {
+            callToActionButton {
+              label
+              destination {
+                ... on Page {
+                  id
+                  uri
+                }
+              }
+            }
             menuItems {
               menuItem {
                 destination {
@@ -50,7 +65,13 @@ export const getStaticProps = async () => {
   });
   return {
     props: {
-      mainMenuItems: mapMainMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
+      mainMenuItems: mapMainMenuItems(
+        data.acfOptionsMainMenu.mainMenu.menuItems
+      ),
+      callToActionLabel:
+        data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
+      callToActionDestination:
+        data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
       blocks: cleanAndTransformBlocks(data?.nodeByUri?.blocks),
     },
   };
